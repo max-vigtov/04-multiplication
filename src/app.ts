c9dff6cd471dc41222ce3cee2877e9c328f7002e
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-07-25 22:08:30 -0600

    Mult: Multiplication table excercise

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
+const fs = require('fs');
 
-const message: string = "Hola mundo";
+let data: string = '';
+const base = 5;
 
-console.log(message);
+for (let index = 0; index < 10; index++) {
+    data += `${base} X ${index + 1} = ${(index + 1) * base}\n`
+}
+
+const outPath = `outputs`
+
+fs.mkdirSync(outPath, { recursive: true});
+fs.writeFile('multiplicationTable.txt', data, (err: any) => {
+  if (err) throw err;
+  console.log('El archivo ha sido guardado!');
+});
